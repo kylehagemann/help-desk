1bf077b3030bb4a0c5adbc938cf0b043d2974926
kylehagemann <kylefhagemann@gmail.com>
2020-01-20 09:13:25 -0600

    Updates

---
+++ precache-manifest.443a8fbaeaca87e02c0787d111216a5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4bbc645e8694ea2b16f2793391813d",
+    "url": "/help-desk/index.html"
+  },
+  {
+    "revision": "be3df6d62bb59068601d",
+    "url": "/help-desk/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9404352dc3f3e960125f",
+    "url": "/help-desk/static/css/main.ace2fc91.chunk.css"
+  },
+  {
+    "revision": "be3df6d62bb59068601d",
+    "url": "/help-desk/static/js/2.80930b8f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/help-desk/static/js/2.80930b8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9404352dc3f3e960125f",
+    "url": "/help-desk/static/js/main.d14d26c1.chunk.js"
+  },
+  {
+    "revision": "9cbba93ecd65e05b8fac",
+    "url": "/help-desk/static/js/runtime-main.c230be0a.js"
+  }
+]);
